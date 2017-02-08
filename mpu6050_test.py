702629f2bb77dd1c68dd06cb7fd5a32b9de36ca8
Jani Hilliaho <jani.hilliaho@gmail.com>
2017-02-08 22:28:14 +0000

    Testing code

--- mpu6050_test.py
+++ mpu6050_test.py
@@ -3,23 +3,27 @@
 # Fixed self.-bug in get_all_data
 from mpu6050 import mpu6050
 
-sensor = mpu6050(0x68)
-
-while 1:
-	#returns [accel, gyro, temp]. Accel and Gyro as {x, y, z} and temp as decimal
-	data = sensor.get_all_data()
-	
-	print(data)
-	print("\n")
-	print("\n")
-
-class imu:
+class IMU:
 	def __init__(self):
 		self.sensor = mpu6050(0x68)
 
 	def getAllData(self):
 		data = sensor.get_all_data()
-		print(data)
+		self.acceleration = data[0]
+		self.gyro = data[1]
+		self.temperature = data[2]
+
+	def getAccelerationData():
+		self.getAllData()
+		return self.acceleration
+
+	def getGyroData():
+		self.getAllData()
+		return self.acceleration
 
 
+if __name__ == "__main__":
+	imu = IMU()
+	print("Acceleration: ", imu.getAccelerationData())
+	print("Gyroscopes: ", imu.getGyroData())
 
