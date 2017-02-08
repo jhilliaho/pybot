cb642f11abf209a41e9b42b2f3186cd129cb7333
Jani Hilliaho <jani.hilliaho@gmail.com>
2017-02-08 21:25:42 +0000

    Testing code

--- examples/hmc5883l.py
+++ examples/hmc5883l.py
@@ -1,100 +1,55 @@
-#!/usr/bin/env python
-# vim: set fileencoding=UTF-8 :
-
-# HMC5888L Magnetometer (Digital Compass) wrapper class
-# Based on https://bitbucket.org/thinkbowl/i2clibraries/src/14683feb0f96,
-# but uses smbus rather than quick2wire and sets some different init
-# params.
-
+#!/usr/bin/python
 import smbus
-import math
 import time
-import sys
+import math
 
-class hmc5883l:
 
-    __scales = {
-        0.88: [0, 0.73],
-        1.30: [1, 0.92],
-        1.90: [2, 1.22],
-        2.50: [3, 1.52],
-        4.00: [4, 2.27],
-        4.70: [5, 2.56],
-        5.60: [6, 3.03],
-        8.10: [7, 4.35],
-    }
+bus = smbus.SMBus(1)
+address = 0x1e
 
-    def __init__(self, port=1, address=0x1E, gauss=1.3, declination=(0,0)):
-        self.bus = smbus.SMBus(port)
-        self.address = address
+openGY87();
 
-        (degrees, minutes) = declination
-        self.__declDegrees = degrees
-        self.__declMinutes = minutes
-        self.__declination = (degrees + minutes / 60) * math.pi / 180
+def read_byte(adr):
+    return bus.read_byte_data(address, adr)
 
-        (reg, self.__scale) = self.__scales[gauss]
-        self.bus.write_byte_data(self.address, 0x00, 0x70) # 8 Average, 15 Hz, normal measurement
-        self.bus.write_byte_data(self.address, 0x01, reg << 5) # Scale
-        self.bus.write_byte_data(self.address, 0x02, 0x00) # Continuous measurement
+def read_word(adr):
+    high = bus.read_byte_data(address, adr)
+    low = bus.read_byte_data(address, adr+1)
+    val = (high << 8) + low
+    return val
 
-    def declination(self):
-        return (self.__declDegrees, self.__declMinutes)
-
-    def twos_complement(self, val, len):
-        # Convert twos compliment to integer
-        if (val & (1 << len - 1)):
-            val = val - (1<<len)
+def read_word_2c(adr):
+    val = read_word(adr)
+    if (val >= 0x8000):
+        return -((65535 - val) + 1)
+    else:
         return val
 
-    def __convert(self, data, offset):
-        val = self.twos_complement(data[offset] << 8 | data[offset+1], 16)
-        if val == -4096: return None
-        return round(val * self.__scale, 4)
+def write_byte(adr, value):
+    bus.write_byte_data(address, adr, value)
 
-    def axes(self):
-        data = self.bus.read_i2c_block_data(self.address, 0x00)
-        #print map(hex, data)
-        x = self.__convert(data, 3)
-        y = self.__convert(data, 7)
-        z = self.__convert(data, 5)
-        return (x,y,z)
+def openGY87():
+	DEVICE_ADDRESS = 0x68
+	DEVICE_REG_MODE1 = 0x00
+	DEVICE_REG_LEDOUT0 = 0x1d
 
-    def heading(self):
-        (x, y, z) = self.axes()
-        headingRad = math.atan2(y, x)
-        headingRad += self.__declination
+	ledout_values = [0x37, 0x02, 0x6A, 0x00, 0x6B, 0x00]
+	bus.write_i2c_block_data(DEVICE_ADDRESS, DEVICE_REG_LEDOUT0, ledout_values)
 
-        # Correct for reversed heading
-        if headingRad < 0:
-            headingRad += 2 * math.pi
 
-        # Check for wrap and compensate
-        elif headingRad > 2 * math.pi:
-            headingRad -= 2 * math.pi
 
-        # Convert to degrees from radians
-        headingDeg = headingRad * 180 / math.pi
-        return headingDeg
+write_byte(0, 0b01110000) # Set to 8 samples @ 15Hz
+write_byte(1, 0b00100000) # 1.3 gain LSb / Gauss 1090 (default)
+write_byte(2, 0b00000000) # Continuous sampling
 
-    def degrees(self, headingDeg):
-        degrees = math.floor(headingDeg)
-        minutes = round((headingDeg - degrees) * 60)
-        return (degrees, minutes)
+scale = 0.92
 
-    def __str__(self):
-        (x, y, z) = self.axes()
-        return "Axis X: " + str(x) + "\n" \
-               "Axis Y: " + str(y) + "\n" \
-               "Axis Z: " + str(z) + "\n" \
-               "Declination: " + self.degrees(self.declination()) + "\n" \
-               "Heading: " + self.degrees(self.heading()) + "\n"
+x_out = read_word_2c(3) * scale
+y_out = read_word_2c(7) * scale
+z_out = read_word_2c(5) * scale
 
-if __name__ == "__main__":
-    # http://magnetic-declination.com/Great%20Britain%20(UK)/Harrogate#
-    compass = hmc5883l(gauss = 4.7, declination = (-2,5))
-    while True:
-        sys.stdout.write("\rHeading: " + str(compass.degrees(compass.heading())) + "     ")
-        sys.stdout.flush()
-        time.sleep(0.5)
+bearing  = math.atan2(y_out, x_out) 
+if (bearing < 0):
+    bearing += 2 * math.pi
 
+print ("Bearing: ", math.degrees(bearing))
