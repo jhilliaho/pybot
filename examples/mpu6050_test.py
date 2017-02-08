349422c61c52bb71b16c3374a3827bd8308acb1e
Jani Hilliaho <jani.hilliaho@gmail.com>
2017-02-08 19:03:51 +0000

    mpu6050 example

--- examples/mpu6050_test.py
+++ examples/mpu6050_test.py
@@ -10,7 +10,7 @@ while 1:
 	data = sensor.get_accel_data()
 	print(data['x'])
 	x.extend([data['x']])
-	if len(x) > 10:
+	if len(x) > 100:
 		x.pop(0)
 
-	print(sum(x)/len(x))
+	print(len(x), "   -   ", sum(x)/len(x))
