f29296cfdb2d151ff7ebab4031e1312d0b7e2065
Jani Hilliaho <jani.hilliaho@gmail.com>
2017-02-08 23:10:12 +0000

    Testing code

--- examples/serial_test.py
+++ examples/serial_test.py
@@ -9,7 +9,7 @@ print(ser.name)         # check which port was really used
 text = ''
 while 1:
 	line = ser.readline()
-	line = line.rstrip().lstrip()
+	line = line.rstrip().lstrip().strip('b')
 	print(line)
 
 ser.close()
