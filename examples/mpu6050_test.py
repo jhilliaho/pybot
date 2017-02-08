79cad00b64271e0392b2d311ae856a05e9ba7166
Jani Hilliaho <jani.hilliaho@gmail.com>
2017-02-08 19:12:37 +0000

    mpu6050 example

--- examples/mpu6050_test.py
+++ examples/mpu6050_test.py
@@ -3,12 +3,7 @@ from mpu6050 import mpu6050
 sensor = mpu6050(0x68)
 
 while 1:
-	data = sensor.get_accel_data(g = True)
+	#returns [accel, gyro, temp]. Accel and Gyro as {x, y, z} and temp as decimal
+	data = sensor.get_all_data()
 	
-	rounded = data
-	
-	rounded['x'] = round(rounded['x'], 2)
-	rounded['y'] = round(rounded['y'], 2)
-	rounded['z'] = round(rounded['z'], 2)
-	
-	print(rounded)
+	print(data)
