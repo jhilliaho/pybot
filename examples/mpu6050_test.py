70e13d346463516ffa9ba8c3f5d9dad7415f0cbf
Jani Hilliaho <jani.hilliaho@gmail.com>
2017-02-08 19:56:27 +0000

    mpu6050 example

--- examples/mpu6050_test.py
+++ examples/mpu6050_test.py
@@ -5,13 +5,19 @@ import smbus
 bus = smbus.SMBus(1)
 address = 0x68
 
-bus.write_byte_data(address, 0x00, 0x37)
-bus.write_byte_data(address, 0x00, 0x02)
-bus.write_byte_data(address, 0x00, 0x6A)
-bus.write_byte_data(address, 0x00, 0x00)
-bus.write_byte_data(address, 0x00, 0x6B)
-bus.write_byte_data(address, 0x00, 0x00)
-#bus.write_byte_data(address, [0x37, 0x02, 0x6A, 0x00, 0x6B, 0x00])
+
+DEVICE_ADDRESS = 0x68
+DEVICE_REG_MODE1 = 0x00
+DEVICE_REG_LEDOUT0 = 0x1d
+
+#Write a single register
+bus.write_byte_data(DEVICE_ADDRESS, DEVICE_REG_MODE1, 0x80)
+
+#Write an array of registers
+ledout_values = [0x37, 0x02, 0x6A, 0x00, 0x6B, 0x00]
+bus.write_i2c_block_data(DEVICE_ADDRESS, DEVICE_REG_LEDOUT0, ledout_values)
+
+
 
 sensor = mpu6050(0x68)
 
