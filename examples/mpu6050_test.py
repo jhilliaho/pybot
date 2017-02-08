2024445de876a8583c1af2730f7c52e05291e573
Jani Hilliaho <jani.hilliaho@gmail.com>
2017-02-08 19:48:57 +0000

    mpu6050 example

--- examples/mpu6050_test.py
+++ examples/mpu6050_test.py
@@ -1,5 +1,12 @@
 from mpu6050 import mpu6050
 
+import smbus
+
+bus = smbus.SMBus(0)
+address = 0x68
+
+bus.write_byte_data(address, [0x37, 0x02, 0x6A, 0x00, 0x6B, 0x00])
+
 sensor = mpu6050(0x68)
 
 while 1:
