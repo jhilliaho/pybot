ac33f4032e831308ddd54b5747005bfb1dc7494b
Jani Hilliaho <jani.hilliaho@gmail.com>
2017-02-08 23:25:50 +0000

    Testing code

--- sensors.py
+++ sensors.py
@@ -1,3 +1,27 @@
 from Compass import Compass
 from IMU import IMU
 from RangeFinder import RangeFinder
+
+class Sensors:
+	def __init__(self):
+		self.compass = Compass()
+		self.IMU = IMU()
+		self.RangeFinder = RangeFinder()
+
+	def updateSensorData(self):
+		self.range = RangeFinder.getRange()
+		self.direction = Compass.getDirection()
+		self.orientation = IMU.getGyroData()
+		self.acceleration = IMU.getAccelerationData()
+
+	def printAllSensorData(self):
+		self.updateSensorData()
+		print("Range: ", self.range)
+		print("Direction: ", self.direction)
+		print("Orientation: ", self.orientation)
+		print("Acceleration: ", self.acceleration)
+
+
+if __name__ == "__main__":
+	sensors = Sensors()
+	sensors.printAllSensorData()
