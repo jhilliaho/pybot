590caec9f1cb86f317881def691a469a33a4d21a
Jani Hilliaho <jani.hilliaho@gmail.com>
2017-02-08 21:50:08 +0000

    Testing code

--- examples/hmc5883l.py
+++ examples/hmc5883l.py
@@ -12,10 +12,10 @@ class compass:
 
 		self.openGY87()
 
-		write_byte(0, 0b01111000) # Set to 8 samples @ 75Hz
-		write_byte(1, 0b00100000) # 1.3 gain LSb / Gauss 1090 (default)
-		write_byte(2, 0b00000000) # Continuous sampling
-		scale = 0.92
+		self.write_byte(0, 0b01111000) # Set to 8 samples @ 75Hz
+		self.write_byte(1, 0b00100000) # 1.3 gain LSb / Gauss 1090 (default)
+		self.write_byte(2, 0b00000000) # Continuous sampling
+		self.scale = 0.92
 
 	def read_byte(self, adr):
 	    return bus.read_byte_data(address, adr)
