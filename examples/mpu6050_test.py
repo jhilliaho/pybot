7e4d210d14368c31fc396927971af818504bbd0c
Jani Hilliaho <jani.hilliaho@gmail.com>
2017-02-08 19:07:58 +0000

    mpu6050 example

--- examples/mpu6050_test.py
+++ examples/mpu6050_test.py
@@ -4,5 +4,11 @@ sensor = mpu6050(0x68)
 
 while 1:
 	data = sensor.get_accel_data()
-	data = round(data, 2)
-	print(data['x'], data['y'], data['z'])
+	
+	rounded = data
+	
+	rounded['x'] = round(rounded['x'], 2)
+	rounded['y'] = round(rounded['y'], 2)
+	rounded['z'] = round(rounded['z'], 2)
+	
+	print(rounded)
