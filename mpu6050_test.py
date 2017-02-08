2245ee5c75e4ef1c09bb70fcb14663cedb7bdd97
Jani Hilliaho <jani.hilliaho@gmail.com>
2017-02-08 22:22:44 +0000

    Testing code

---
+++ mpu6050_test.py
@@ -0,0 +1,25 @@
+# https://github.com/Tijndagamer/mpu6050
+# pip install mpu6050-raspberrypi
+# Fixed self.-bug in get_all_data
+from mpu6050 import mpu6050
+
+sensor = mpu6050(0x68)
+
+while 1:
+	#returns [accel, gyro, temp]. Accel and Gyro as {x, y, z} and temp as decimal
+	data = sensor.get_all_data()
+	
+	print(data)
+	print("\r\n")
+	print("\r\n")
+
+class imu:
+	def __init__(self):
+		self.sensor = mpu6050(0x68)
+
+	def getAllData(self):
+		data = sensor.get_all_data()
+		print(data)
+
+
+
