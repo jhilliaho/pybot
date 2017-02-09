07f925d25509e1241af22de98b57054a1117626a
Jani Hilliaho <jani.hilliaho@gmail.com>
2017-02-09 00:49:38 +0000

    Code tests

--- RangeFinder.py
+++ RangeFinder.py
@@ -2,18 +2,28 @@ import serial
 
 class RangeFinder:
 	def __init__(self):
-		self.device = serial.Serial(port='/dev/ttyUSB0', baudrate=115200, timeout=0.1)
+		self.device = serial.Serial(port='/dev/cu.usbserial-AM01TQUH', baudrate=115200, timeout=0.1)
 
 	def getRange(self):
-		line = self.device.readline()
-		line = int(line)
+		self.device.flush()
+		while True:
+			line = self.device.readline()
+			try: 
+				line = int(line)
+				if line < 20:
+					continue
+				else:
+					break
+			except ValueError:
+				print("RangeFinder: Trying to get proper range")
+				continue
 		return line
 
 	def closeSerial(self):
 		self.device.close()
 
-
 if __name__ == "__main__":
 	dev = RangeFinder()
-	print(dev.getRange())
+	for i in range(50):
+		print(dev.getRange())
 	dev.closeSerial()
