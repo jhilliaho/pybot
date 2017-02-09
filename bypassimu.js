5ac0a2cddd95e3c7fb46bd020465476533606141
Jani Hilliaho <jani.hilliaho@gmail.com>
2017-02-09 21:45:50 +0000

    Modified imu-bypass

--- bypassimu.js
+++ bypassimu.js
@@ -9,12 +9,11 @@ board.on("ready", function() {
 	board.io.i2cConfig();
 	board.io.i2cWrite(0x68, [0x37, 0x02, 0x6A, 0x00, 0x6B, 0x00]);
 
-	// Configurate the imu module
-	board.io.i2cWrite(0x68, [0x37, 0x02, 0x6A, 0x00, 0x6B, 0x00]);
-
 	var imu = new five.IMU({controller: "MPU6050"});
+	
 	var compass = new five.Compass({controller: "HMC5883L"});	
 
+	/*
 	var lidar = new SerialPort("/dev/ttyUSB0", {
 		baudRate: 115200,
 		parser: SerialPort.parsers.readline('\r\n')
@@ -33,7 +32,8 @@ board.on("ready", function() {
 		num = parseInt(num);
 		console.log("LIDAR: ", num);
 	});
-
+	*/
+	
 	console.log("Lidar, imu and compass initialized");
 
 });
