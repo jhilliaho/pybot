1778502664736b8f703e1291799c9a757b549dd5
Jani Hilliaho <jani.hilliaho@gmail.com>
2017-02-10 21:52:37 +0000

    Main program

--- Compass.py
+++ Compass.py
@@ -47,11 +47,11 @@ class Compass:
 		self.bus.write_byte_data(DEVICE_ADDRESS, 0x6A, 0x00)
 		self.bus.write_byte_data(DEVICE_ADDRESS, 0x6B, 0x00)
 		self.bus.write_byte_data(DEVICE_ADDRESS, 0x37, 0x02)
-		self.bus.write_byte_data(DEVICE_ADDRESS, 0x1A, 0x03)
+		self.bus.write_byte_data(DEVICE_ADDRESS, 0x1A, 0x04)
 
 		# 1: Min: -20.1 Max: 78.89 Mean: 7.52 Std: 20.34
 		# 2: Min: -10.54 Max: 20.88 Mean: 3.02 Std: 7.6
-		# 3: 
+		# 3: Min: -2.85 Max: 12.09 Mean: 2.54 Std: 3.01
 		# 4: 
 		# 5: 
 		# 6: 
