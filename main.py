11e736b0ffa4eafea4a6ca82a8b9b3ec4a7e21cf
Jani Hilliaho <jani.hilliaho@gmail.com>
2017-02-10 22:24:48 +0000

    Main program

--- main.py
+++ main.py
@@ -14,7 +14,7 @@ print("Alles gut")
 while True:
 	acc = sensors.getAccelerationData()
 	pitch = acc['pitch']
-	pitch = int(20 * pitch) - 65
+	pitch = int(50 * pitch) - 162
 
 	print("PITCH: " + str(pitch))
 	if abs(pitch) > 20:
