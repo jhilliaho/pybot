59232de9f29b8e01aebb5acabfa0c9dbd10e13c4
Jani Hilliaho <jani.hilliaho@gmail.com>
2017-02-10 15:59:02 +0000

    Roll and pitch

--- sensors.py
+++ sensors.py
@@ -1,15 +1,15 @@
 from Compass import Compass
 from IMU import IMU
-from RangeFinder import RangeFinder
+#from RangeFinder import RangeFinder
 
 class Sensors:
 	def __init__(self):
 		self.Compass = Compass()
 		self.IMU = IMU()
-		self.RangeFinder = RangeFinder()
+		#self.RangeFinder = RangeFinder()
 
 	def updateSensorData(self):
-		self.range = self.RangeFinder.getRange()
+		#self.range = self.RangeFinder.getRange()
 		self.direction = self.Compass.getDirection()
 		self.orientation = self.IMU.getGyroData()
 		self.acceleration = self.IMU.getAccelerationData()
@@ -17,7 +17,7 @@ class Sensors:
 	def printAllSensorData(self):
 		self.updateSensorData()
 		
-		print("Range: ", self.range)
+		#print("Range: ", self.range)
 		print("Direction: ", self.direction)
 		print("Roll: ", self.acceleration['roll'])
 		print("Pitch: ", self.acceleration['pitch'])
