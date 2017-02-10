d973ba672cdfb059afc131e923a065d6015bbe7d
Jani Hilliaho <jani.hilliaho@gmail.com>
2017-02-10 22:21:20 +0000

    Main program

--- main.py
+++ main.py
@@ -10,16 +10,12 @@ sensors.printAllSensorData()
 
 print("Alles gut")
 
-min = 0
-max = 0
 
 while True:
 	acc = sensors.getAccelerationData()
 	pitch = acc['pitch']
 	pitch = int(40 * pitch) - 127
-	if pitch < min:
-		min = pitch
-	if pitch > max:
-		max = pitch
-	print("PITCH: " + str(pitch) + " MIN: " + str(min) + " MAX: " + str(max))
-	#motors.setSpeeds(-pitch, -pitch)
+
+	print("PITCH: " + str(pitch))
+	if abs(pitch) > 5
+		motors.setSpeeds(-pitch, -pitch)
