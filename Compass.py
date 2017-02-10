7997302b89811daa980325443390f5ef8af45c86
Jani Hilliaho <jani.hilliaho@gmail.com>
2017-02-10 21:53:18 +0000

    Main program

--- Compass.py
+++ Compass.py
@@ -47,13 +47,13 @@ class Compass:
 		self.bus.write_byte_data(DEVICE_ADDRESS, 0x6A, 0x00)
 		self.bus.write_byte_data(DEVICE_ADDRESS, 0x6B, 0x00)
 		self.bus.write_byte_data(DEVICE_ADDRESS, 0x37, 0x02)
-		self.bus.write_byte_data(DEVICE_ADDRESS, 0x1A, 0x05)
+		self.bus.write_byte_data(DEVICE_ADDRESS, 0x1A, 0x06)
 
 		# 1: Min: -20.1 Max: 78.89 Mean: 7.52 Std: 20.34
 		# 2: Min: -10.54 Max: 20.88 Mean: 3.02 Std: 7.6
 		# 3: Min: -2.85 Max: 12.09 Mean: 2.54 Std: 3.01
 		# 4: Min: -0.63 Max: 5.43 Mean: 2.37 Std: 1.53
-		# 5: 
+		# 5: Min: 0.85 Max: 3.83 Mean: 2.5 Std: 0.8
 		# 6: 
 
 	def getDirection(self):
