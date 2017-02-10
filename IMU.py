25c6581771eda8343f0982148930d7082b74913b
Jani Hilliaho <jani.hilliaho@gmail.com>
2017-02-10 15:02:35 +0000

    Roll and pitch

--- IMU.py
+++ IMU.py
@@ -2,6 +2,7 @@
 # pip install mpu6050-raspberrypi
 # Fixed self.-bug in get_all_data
 from mpu6050 import mpu6050
+import math
 
 class IMU:
 	def __init__(self):
@@ -12,6 +13,13 @@ class IMU:
 		self.acceleration = data[0]
 		self.gyro = data[1]
 		self.temperature = data[2]
+
+		x = self.acceleration.x
+		y = self.acceleration.y
+		z = self.acceleration.z
+
+		self.acceleration.roll = math.atan2(y, z) * 180/math.pi;
+		self.acceleration.pitch = math.atan2(-x, sqrt(y*y + z*z)) * 180/math.pi;
 
 	def getAccelerationData(self):
 		self.getAllData()
