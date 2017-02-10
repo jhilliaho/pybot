0fd2d266ef3b2962583d20f5239b4ec87ae572f4
Jani Hilliaho <jani.hilliaho@gmail.com>
2017-02-10 23:41:19 +0000

    Main program

--- main.py
+++ main.py
@@ -28,7 +28,7 @@ while True:
 	pitch = sensors.getAccelerationData()['pitch'] - pitchCalibration
 	if pitch < 0:
 		pitchstr = "{:.3f}".format(pitch) 
-	else
+	else:
 		pitchstr = " {:.3f}".format(pitch) 		
 	print("PITCH: " + pitchstr)
 	if abs(pitch) > 0.3:
