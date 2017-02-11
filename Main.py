7a1b061cf9e4d5d706d15b165e76699e5e8fefca
Jani Hilliaho <jani.hilliaho@gmail.com>
2017-02-11 13:59:37 +0000

    main

---
+++ Main.py
@@ -0,0 +1,37 @@
+from Sensors import Sensors
+from MotorDriver import MotorDriver
+import Server
+
+import time
+
+sensors = Sensors()
+motors = MotorDriver()
+
+sensors.printAllSensorData()
+
+print("Alles gut")
+
+pitchCalibration = 0
+
+def calibrate():
+	global pitchCalibration
+	sum = 0
+	for i in range(100):
+		sum += sensors.getAccelerationData()['pitch']
+	pitchCalibration = sum/100
+
+calibrate()
+print("Calibration: " + str(pitchCalibration))
+
+
+
+while True:
+	pitch = sensors.getAccelerationData()['pitch'] - pitchCalibration
+	if pitch < 0:
+		pitchstr = "{:.3f}".format(pitch) 
+	else:
+		pitchstr = " {:.3f}".format(pitch) 		
+	print("PITCH: " + pitchstr)
+	print("CONTROLLER: " + Server.controllerData)
+	if abs(pitch) > 0.3:
+		motors.setSpeeds(-20 * pitch, -20 * pitch)
