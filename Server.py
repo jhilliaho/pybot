e5fb3cbb72108f2679ea01fb7f46fd5682006952
Jani Hilliaho <jani.hilliaho@gmail.com>
2017-02-11 13:46:28 +0000

    Connectivity

--- Server.py
+++ Server.py
@@ -3,32 +3,40 @@ import eventlet
 import eventlet.wsgi
 from flask import Flask, render_template
 import os
-sio = socketio.Server()
 
-template_dir = os.path.abspath('.')
-app = Flask(__name__, template_folder=template_dir)
 
-@app.route('/')
-def index():
-    """Serve the client-side application."""
-    return render_template('index.html')
+class Server:
 
-@sio.on('connect')
-def connect(sid, environ):
-    print("connect ", sid)
+	def __init__(self):
 
-@sio.on('controllerDataFromBrowser')
-def message(sid, data):
-    print("message ", data)
-    sio.emit('reply', room=sid)
 
-@sio.on('disconnect')
-def disconnect(sid):
-    print('disconnect ', sid)
+	self.sio = socketio.Server()
+
+	template_dir = os.path.abspath('.')
+	self.app = Flask(__name__, template_folder=template_dir)
+
+    self.app = socketio.Middleware(self.sio, self.app)
+
+    eventlet.wsgi.server(eventlet.listen(('', 8000)), self.app)
+
+	@self.app.route('/')
+	def index(self):
+	    """Serve the client-side application."""
+	    return render_template('index.html')
+
+	@self.sio.on('connect')
+	def connect(self, sid, environ):
+	    print("connect ", sid, environ)
+
+	@self.sio.on('controllerDataFromBrowser')
+	def message(self, sid, data):
+	    print("message ", data)
+	    #sio.emit('reply')
+
+	@self.sio.on('disconnect')
+	def disconnect(self, sid):
+	    print('disconnect ', sid)
 
 if __name__ == '__main__':
-    # wrap Flask application with engineio's middleware
-    app = socketio.Middleware(sio, app)
-
-    # deploy as an eventlet WSGI server
-    eventlet.wsgi.server(eventlet.listen(('', 8000)), app)
+	ser = Server()
+	
