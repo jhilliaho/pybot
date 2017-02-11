b8a4010e8d717c32ff0d7d666cc044a9b7544ff4
Jani Hilliaho <jani.hilliaho@gmail.com>
2017-02-11 15:10:31 +0000

    Threading

---
+++ Utilities.py
@@ -0,0 +1,18 @@
+
+def float2str(val, precision=1, length=7):
+	if val < 0:
+		flstr = ("{:." + str(precision) + "f}").format(val) 
+	else:
+		flstr = (" {:." + str(precision) + "f}").format(val)
+	while len(flstr) < length:
+		flstr += " "
+	return flstr
+
+def int2str(val, length=5):
+	if val < 0:
+		instr = str(val)
+	else:
+		instr = " " + str(val)
+	while len(instr) < length:
+		instr += " "
+	return instr	
