55995b0f99b1b0177ad725900cb7c6dd6bebed31
Jani Hilliaho <jani.hilliaho@gmail.com>
2017-02-11 14:04:41 +0000

    Threading

--- Main.py
+++ Main.py
@@ -1,37 +1,58 @@
 from Sensors import Sensors
 from MotorDriver import MotorDriver
-import Server
 
 import time
+import threading
 
 sensors = Sensors()
 motors = MotorDriver()
-
 sensors.printAllSensorData()
-
 print("Alles gut")
 
-pitchCalibration = 0
-
-def calibrate():
-	global pitchCalibration
-	sum = 0
-	for i in range(100):
-		sum += sensors.getAccelerationData()['pitch']
-	pitchCalibration = sum/100
-
-calibrate()
-print("Calibration: " + str(pitchCalibration))
 
 
+# Global Server variable
+Server = None
+class threadOne(threading.Thread): #I don't understand this or the next line
+    def run(self):
+    	print("Server thread running")
+        global Server
+        import Server
 
-while True:
-	pitch = sensors.getAccelerationData()['pitch'] - pitchCalibration
-	if pitch < 0:
-		pitchstr = "{:.3f}".format(pitch) 
-	else:
-		pitchstr = " {:.3f}".format(pitch) 		
-	print("PITCH: " + pitchstr)
-	print("CONTROLLER: " + Server.controllerData)
-	if abs(pitch) > 0.3:
-		motors.setSpeeds(-20 * pitch, -20 * pitch)
+class threadTwo(threading.Thread):
+    def run(self):
+    	print("Main thread running")
+
+    	global sensors
+    	global motors
+
+		pitchCalibration = 0
+
+		def calibrate():
+			global pitchCalibration
+			sum = 0
+			for i in range(100):
+				sum += sensors.getAccelerationData()['pitch']
+			pitchCalibration = sum/100
+
+		calibrate()
+		print("Calibration: " + str(pitchCalibration))
+
+
+
+		while True:
+			pitch = sensors.getAccelerationData()['pitch'] - pitchCalibration
+			if pitch < 0:
+				pitchstr = "{:.3f}".format(pitch) 
+			else:
+				pitchstr = " {:.3f}".format(pitch) 		
+			print("PITCH: " + pitchstr)
+			print("CONTROLLER: " + Server.controllerData)
+			if abs(pitch) > 0.3:
+				motors.setSpeeds(-20 * pitch, -20 * pitch)
+
+
+
+
+threadOne().start()
+threadTwo().start()
